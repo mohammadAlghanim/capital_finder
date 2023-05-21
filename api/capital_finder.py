2e3902d7146edf4e215fb4dc4a2dbde024b255d9
mohammadAlghanim <ghanimmohammad11@gmail.com>
2023-05-21 14:13:17 +0000

    lab16

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,23 +1,32 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-# import requests
+import requests
  
 class handler(BaseHTTPRequestHandler):
- 
+  # result = None 
   def do_GET(self):
-    # s = self.path
-    # url_components = parse.urlsplit(s)
-    # query_strings_list = parse.parse_qsl(url_components.query)
-    # dic = dict(query_strings_list)
-    # word = dic.get("word")
+    s = self.path
+    url_components = parse.urlsplit(s)
+    query_strings_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_strings_list)
+    country = dic.get("country")
+    capital = dic.get("capital")
+    
+    if capital:
+      url ="https://restcountries.com/v3.1/capital/"
+      res = requests.get(url+capital)
+      data = res.json()
+      res = data[0]["name"]["common"]
+      res2 = f"{capital} is the capital of {res}.."
+    elif country:
+      url ="https://restcountries.com/v3.1/name/"
+      res = requests.get(url+country)
+      data = res.json()
+      res = data[0]["capital"][0]
+      res2 = f"The capital of {country} is {res}"
 
-    # if word:
-    #     url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-    #     res = requests.get(url+word)
-    #     data = res.json()
-        # result = data[0]["meanings"][0]["definitions"][0]["definition"]
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write("helkm;lmollo".encode())
+    self.wfile.write(res2.encode('utf-8'))
     return
